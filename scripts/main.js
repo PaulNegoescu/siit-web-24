cd6d431c48fe492a055f1f9e1102a9f222381c81
Paul Negoescu <paul.negoescu@nagarro.com>
2022-06-14 21:08:54 +0300

    Adding todos

--- scripts/main.js
+++ scripts/main.js
@@ -6,24 +6,31 @@
 // in li trebuie sa facem si celelalte elemente
 
 function todoList() {
+  const apiUrl = 'http://localhost:3005/todos';
+
+  function buildListItem(todo) {
+    const item = document.createElement('li');
+
+    const label = document.createElement('label');
+    const check = document.createElement('input');
+    check.type = 'checkbox';
+    label.append(check, todo.title);
+
+    const delBtn = document.createElement('button');
+    const icon = document.createElement('i');
+    delBtn.append(icon);
+    icon.classList.add('fa-solid', 'fa-trash-can');
+
+    item.append(label, delBtn);
+
+    return item;
+  }
+
   function buildHtmlList(todos) {
     const list = document.createDocumentFragment();
 
     for (const todo of todos) {
-      const item = document.createElement('li');
-
-      const label = document.createElement('label');
-      const check = document.createElement('input');
-      check.type = 'checkbox';
-      label.append(check, todo.title);
-
-      const delBtn = document.createElement('button');
-      const icon = document.createElement('i');
-      delBtn.append(icon);
-      icon.classList.add('fa-solid', 'fa-trash-can');
-
-      item.append(label, delBtn);
-
+      const item = buildListItem(todo);
       list.append(item);
     }
 
@@ -40,9 +47,53 @@ function todoList() {
     displayListInPage(list);
   }
 
-  fetch('http://localhost:3005/todos')
+  fetch(apiUrl)
     .then((res) => res.json())
     .then(displayTodos);
+
+  // sa adaugam un todo nou via form din html
+  // listen to a submit event on the form -> addEventListener('submit')
+  // trebuie sa aflam valoarea pe care a introdus-o utilizatorul -> querySelector(<input>).value
+  // trimitem valoarea la server -> fetch(url, {method: POST, body:, headers:})
+  // we need to refresh the list
+  const addTodoForm = document.querySelector('[data-add-todo-form]');
+  const errorMessageContainer = document.querySelector('[data-error-message]');
+  const formInputs = addTodoForm.elements;
+
+  addTodoForm.addEventListener('submit', handleAddTodo);
+
+  formInputs.title.addEventListener('keydown', handleKeydown);
+
+  function handleKeydown() {
+    errorMessageContainer.innerText = '';
+  }
+
+  function handleAddTodo(e) {
+    e.preventDefault(); // am oprit comportamentul implicit al formularului
+    // const title = document.querySelector('[data-todo-title-input]').value;
+    const title = formInputs.title.value;
+    errorMessageContainer.innerText = '';
+
+    if (!title.trim()) {
+      errorMessageContainer.innerText =
+        'Please enter a non-empty value for the todo title.';
+      return;
+    }
+
+    fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        title,
+        completed: false,
+        userId: 1,
+      }),
+    })
+      .then((res) => res.json())
+      .then(console.log);
+  }
 }
 
 todoList();
