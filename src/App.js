ea3bb59103c92041d8ab1f4e1fa5f09e2a780963
Paul Negoescu <paul.negoescu@nagarro.com>
2022-07-19 21:06:21 +0300

    React router and forms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 import { Counter } from './features/Counter';
 import { Weather } from './features/Weather';
 
 function App() {
   return (
-    <>
-      <Counter />
-      <Weather city="Bucharest" />
-    </>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/counter">Counter</NavLink>
+          </li>
+          <li>
+            <NavLink to="/weather">Weather</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/weather" element={<Weather city="Timisoara" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/features/Weather/Weather.js
+++ src/features/Weather/Weather.js
@@ -2,6 +2,17 @@ import { useEffect, useState } from 'react';
 
 export function Weather({ city }) {
   const [weather, setWeather] = useState(null);
+  const [values, setValues] = useState({
+    city,
+    country: 'RO',
+  });
+
+  useEffect(() => {
+    // setValues({ ...values, city });
+    setValues((oldState) => {
+      return { ...oldState, city };
+    });
+  }, [city]);
 
   useEffect(() => {
     fetch(
@@ -17,9 +28,52 @@ export function Weather({ city }) {
 
   const currentTemp = (weather?.main.temp - 273.15).toFixed(1);
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${values.city},${values.country}&appid=c7da641777760054e5ca6164eb47580a`
+    )
+      .then((res) => res.json())
+      .then((data) => setWeather(data));
+  }
+
+  function handleInputChange(e) {
+    // const newValues = { ...values };
+    // newValues[e.target.name] = e.target.value;
+    // setValues(newValues);
+
+    setValues({ ...values, [e.target.name]: e.target.value });
+  }
+
   return (
     <>
       <h1>Weather</h1>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label htmlFor="city">City</label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            value={values.city}
+            onChange={handleInputChange}
+          />
+        </p>
+        <p>
+          <label htmlFor="country">Country</label>
+          <select
+            name="country"
+            id="country"
+            value={values.country}
+            onChange={handleInputChange}
+          >
+            <option value="RO">Romania</option>
+            <option value="DE">Germania</option>
+            <option value="US">Statele Unite</option>
+          </select>
+        </p>
+        <button type="submit">Get Weather</button>
+      </form>
       {weather && currentTemp} &deg;C
     </>
   );
